e1e37e21c98ace7b716794b932edac51134dce6d
kdr <johannes.hinkov@gmail.com>
2021-10-10 22:17:05 +0200

    add docker for binary setup

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -r ./binaries/dist; mkdir ./binaries/dist
-unzip -o -d ./binaries ./binaries/dist.zip
+rm -r ./fe/binaries/dist; mkdir ./fe/binaries/dist
+unzip -o -d ./fe/binaries ./fe/binaries/dist.zip
 docker-compose -f docker-compose-binaries.yml up -d
